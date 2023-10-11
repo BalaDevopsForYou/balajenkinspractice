97891253f7a6a967289f9de3acf0e91d571db03c
BalaDevopsForYou <145206154+BalaDevopsForYou@users.noreply.github.com>
2023-10-11 16:41:21 +0100

    Update Program.cs

--- bkfordevops/bkfordevops/Program.cs
+++ bkfordevops/bkfordevops/Program.cs
@@ -1,3 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+int a= Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("addition would be "+ (a+b));
+Console.WriteLine("subtraction would be " + (a - b));
+Console.WriteLine("Multiplication would be " + (a * b));
+
 Console.ReadLine();
