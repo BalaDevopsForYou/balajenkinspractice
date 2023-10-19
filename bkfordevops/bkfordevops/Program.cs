b054cb492581e41e0329053b4abd5ae9ab822736
BalaDevopsForYou <145206154+BalaDevopsForYou@users.noreply.github.com>
2023-10-19 21:26:20 +0100

    Update Program.cs
    this is for webhook trigger in jenkins

--- bkfordevops/bkfordevops/Program.cs
+++ bkfordevops/bkfordevops/Program.cs
@@ -1,5 +1,6 @@
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, bro whatsup!");
 int a= Convert.ToInt32(Console.ReadLine());
 int b = Convert.ToInt32(Console.ReadLine());
 
